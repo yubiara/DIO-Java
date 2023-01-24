0c53fc54ca15024cfe050604c79859cfc3b949c4
Yubiara <yubiara@hotmail.com>
2023-01-23 22:02:02 -0300

    Executando via terminal

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Dio-Bootcamp-QA-Java
 Repositorio para armazenar todos os códigos em Java criados durante o bootcamp de QA
+
+## Códigos serão todos inserido no mesmo repositório
---
+++ tipos-variaveis/src/MinhaClasse.java
@@ -0,0 +1,8 @@
+public class MinhaClasse {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo Terminal");
+    }
+}
