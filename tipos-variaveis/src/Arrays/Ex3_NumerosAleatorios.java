772fc9744995254a7ad5aae4bfd0bd6a825df070
Yubiara <yubiara@hotmail.com>
2023-01-26 20:01:16 -0300

    Exercicios de ARRAYS

---
+++ tipos-variaveis/src/Arrays/Ex3_NumerosAleatorios.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+import java.util.Random;
+
+/*
+Faça um Programa que leia 20 números inteiros aleatórios (entre 0 e 100) armazene-os num vetor.
+Ao final mostre os números e seus sucessores.
+*/
+public class Ex3_NumerosAleatorios {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+
+        System.out.println("Numeros alertorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println(numero + " ");
+        }
+
+        System.out.println("Sucessores dos aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println((numero + 1) + " ");
+
+        }
+
+    }
+
+}
